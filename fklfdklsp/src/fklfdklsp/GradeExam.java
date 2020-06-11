b3e1e411f31f2615730b6ba409a655453e7217e4
csw <leeg0617@naver.com>
2020-06-11 09:44:09 +0900

    06-11

--- fklfdklsp/src/fklfdklsp/GradeExam.java
+++ fklfdklsp/src/fklfdklsp/GradeExam.java
@@ -1,10 +1,25 @@
 package fklfdklsp;
-
+import java.util.Scanner;
 public class GradeExam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.println("수학, 과학, 영어");
+		Grade me=new Grade();
+		me.math=scn.nextInt();
+		me.science=scn.nextInt();
+		me.english=scn.nextInt();
+		System.out.println("평균"+me.average());
 	}
 
+}
+class Grade{
+	int math;
+	int science;
+	int english;
+	
+	public class average(){
+		return (math+science+english/3);
+	}
 }
