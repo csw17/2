e71898dbd079136ae2f42463bc50e13c1dd51267
user <user@asd06>
2020-06-16 09:40:20 +0900

    20-06-16

---
+++ fklfdklsp/src/fklfdklsp/trianglexam.java
@@ -0,0 +1,31 @@
+package fklfdklsp;
+
+
+public class trianglexam{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		triangle tr=new triangle(10.2,17.3);
+			System.out.println("»ï°¢ÇüÀÇ ³ÐÀÌ:"+tr.getarea());
+		tr.setsize(7.9, 9.2);
+		System.out.println("»ï°¢ÇüÀÇ ³ÐÀÌ"+tr.getarea());
+	}
+
+}
+
+class triangle {
+	double bottom;
+	double height;
+	public triangle(double bt, double hg) {
+		bottom=bt;
+		height=hg;
+	}
+	public void setsize(double bt, double hg) {
+		bottom=bt;
+		height=hg;
+	}
+	public double getarea() {
+		return bottom*height/2;
+	}
+}
+	
